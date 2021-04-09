99888f6bad8c250c255b93ea89c2841d8054956c
ProtikAcharjay <82051598+ProtikAcharjay@users.noreply.github.com>
2021-04-10 00:19:45 +0600

    Add files via upload

---
+++ 18.usingpythonandmodule.py
@@ -0,0 +1,8 @@
+import random
+random_number= random.randint(0,100)
+print(random_number)
+rand= random.random()*50
+print(rand)
+lst=["protik","amartya","niloy","jion","bondhon","jayem"]
+c=random.choice(lst)
+print(c)
