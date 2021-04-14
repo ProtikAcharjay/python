909c9788bf2afd024e88bee061f81ebeb5933d4f
ProtikAcharjay <82051598+ProtikAcharjay@users.noreply.github.com>
2021-04-14 19:20:23 +0600

    Add files via upload

---
+++ 23.makePythonTalk.py
@@ -0,0 +1,7 @@
+def speak(str):
+    from win32com.client import Dispatch
+    speak=Dispatch("sapi.spvoice")
+    speak.speak(str)
+if __name__ == '__main__':
+    speak("Protik the best great don in the world")
+    speak("Thanks Buddy")
